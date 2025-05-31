644f968a206baacaf38ac186de8684a2e3b61009
Hoang-debug <HoangAnh.Do984@schools.sa.edu.au>
2025-05-31 20:40:24 +0930

    Initial commit

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template
+import psycopg2
+import os
 
 app = Flask(__name__)
-messages = []
 
-@app.route('/', methods=['GET'])
+# PostgreSQL credentials (use environment vars on Render)
+conn = psycopg2.connect(
+    host=os.environ['DB_HOST'],
+    dbname=os.environ['DB_NAME'],
+    user=os.environ['DB_USER'],
+    password=os.environ['DB_PASSWORD'],
+)
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html', messages=messages)
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    message = request.form.get('message')
-    if message:
-        messages.append(message)
-    return redirect('/')
+    cur = conn.cursor()
+    if request.method == "POST":
+        message = request.form["message"]
+        cur.execute("INSERT INTO messages (text) VALUES (%s)", (message,))
+        conn.commit()
+    
+    cur.execute("SELECT text FROM messages ORDER BY id DESC")
+    rows = cur.fetchall()
+    return render_template("index.html", messages=rows)
